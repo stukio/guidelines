64cdb1b3d6044ebc57b553d0d712a692f0b44f0d
Tiago Martins <tiagom87@gmail.com>
2015-10-06 18:15:08 +0100

    Generated tasks state migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151006164014) do
+ActiveRecord::Schema.define(version: 20151006171050) do
 
   create_table "tasks", force: :cascade do |t|
     t.string   "content"
     t.integer  "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "state"
   end
 
   create_table "users", force: :cascade do |t|
